dd215edc8c375d0e065c241bd461b6b1dec02022
AvikDAS2001 <avik.das.3236@gmail.com>
2025-11-01 23:43:08 +0530

    Initial commit

---
+++ order-inventory-system/src/test/java/com/projects/order_inventory_system/OrderInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.order_inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
