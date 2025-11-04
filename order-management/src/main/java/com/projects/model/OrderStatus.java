2c8a011575a11acd56db82f9b52f81da5e5d6fcf
AvikDAS2001 <avik.das.3236@gmail.com>
2025-11-05 00:24:03 +0530

    adding all the layers for order service

---
+++ order-management/src/main/java/com/projects/model/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.projects.model;
+
+public class OrderStatus {
+}
---
+++ order-management/src/main/java/com/projects/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.projects.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+}
